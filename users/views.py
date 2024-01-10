be22fd1c9f371acfa61cdb1425203783bb4bf750
Banywick <android19377@gmail.com>
2024-01-10 11:55:01 +0300

    объеденил Дениса логику с версткой

--- users/views.py
+++ users/views.py
@@ -6,13 +6,14 @@ from .forms import UserRegisterForm
 
 def register(request):
     if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-    else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+        print(request.POST)
+    #     form = UserRegisterForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect("login")
+    # else:
+    #     form = UserRegisterForm()
+        return render(request, 'users/profile.html')
 
 
 @login_required
