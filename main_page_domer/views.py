85e6bf77325b5c23f433f0b4167929f9e9e0788c
sveta-alisa <svetaastashova@gmail.com>
2024-01-15 12:30:53 +0300

    task 281 completed

--- main_page_domer/views.py
+++ main_page_domer/views.py
@@ -13,4 +13,16 @@ def get_personal_account_page(request):
     return render(request, 'personal_account.html')
 
 def get_user_data_page(request):
-    return  render(request, 'user_data.html')
+    return render(request, 'user_data.html')
+
+def get_incoming_page(request):
+    return render(request, 'incoming_messages.html')
+
+def get_outgiong_page(request):
+    return render(request, 'outgiong_messages.html')
+
+def get_sent_page(request):
+    return render(request, 'sent_messages.html')
+
+def get_admin_message_page(request):
+    return render(request, 'admin_message.html')
