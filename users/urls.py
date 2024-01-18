cb85e9920c3e8658d8ecc05e1634dd309a4e3765
Banywick <android19377@gmail.com>
2024-01-18 14:21:16 +0300

    email console backend done + css

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
 from django.urls import path, reverse_lazy
-from django.contrib.auth import views as auth_views
+
 from .views import profile, login_view
 
 app_name = 'users'
@@ -9,22 +11,26 @@ urlpatterns = [
     # path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(
-        template_name='base.html',
-        email_template_name='users:password_reset_email',
-        success_url=reverse_lazy('users:password_reset_done')
-    ),
+    path('password-reset/',
+         PasswordResetView.as_view(
+             template_name="base.html",
+             email_template_name="users/password_reset_email.html",
+             success_url=reverse_lazy("users:password_reset_done")
+         ),
          name='password_reset'),
 
     path('reset_password_sent/',
-         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
          name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='users/password_reset_confirm.html',
-        success_url=reverse_lazy('users:password_reset_complete')
-    ), name='password_reset_confirm'),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html",
+             success_url=reverse_lazy("users:password_reset_complete")
+         ),
+         name='password_reset_confirm'),
 
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password_complete/', PasswordResetCompleteView.as_view(
+        template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -12,24 +12,26 @@ from .forms import UserRegisterForm
 
 
 def login_view(request):
-    email = request.POST['email']
-    password = request.POST['password']
-    user = authenticate(request, email=email, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponse('Вы успешно вошли в систему!')
-    else:
-        return HttpResponse('Неверный логин или пароль.')
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse('Вы успешно вошли в систему!')
+        else:
+            return HttpResponse('Неверный логин или пароль.')
+    return render(request, 'registration/login.html')
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         username = request.POST['contact']
-#         email = request.POST['email']
-#         password = request.POST['password']
-#         repeat_password = request.POST['repeat_password']
-#         user = User(first_name=username, email=email, password=password)
-#         user.set_password()
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['contact']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeat_password = request.POST['repeat_password']
+        user = User(first_name=username, email=email, password=password)
+        user.set_password()
 
 
 # def reset_password(request):
