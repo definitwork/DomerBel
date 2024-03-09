500d57c1a822292d66a909789408b2cf85d450e3
Alexander Povedaiko <a.povedaiko@yandex.ru>
2024-03-09 14:46:19 +0300

    add breadcrumbs region and category

--- advertisement/utils.py
+++ advertisement/utils.py
@@ -1,18 +1,20 @@
 from django.core.paginator import Paginator
 
+from advertisement.models import Region
+
 
 def sorted_by_number(number):
-    if (number == '30'
-            or number == '60'
-            or number == '90'):
+    if (number == '3'
+            or number == '6'
+            or number == '9'):
         sort_for_paginator = int(number)
         return sort_for_paginator
     else:
-        return 30
+        return 3
 
 
-def variables_for_paginator(queryset, page=1, elments=30):
-    paginator = Paginator(queryset, elments)
+def variables_for_paginator(queryset, page=1, elements=30):
+    paginator = Paginator(queryset, elements)
     page_number = page
     page_obj = paginator.get_page(page_number)
     return page_obj
@@ -21,14 +23,14 @@ def variables_for_paginator(queryset, page=1, elments=30):
 def sorted_by_date_or_price(sort):
     if sort.get('date'):
         if sort['date'] == '0':
-            return (1, 'date_of_create')
+            return 1, 'date_of_create'
         else:
-            return (0, '-date_of_create')
+            return 0, '-date_of_create'
     elif sort.get('price'):
         if sort['price'] == '0':
-            return (1, 'price')
+            return 1, 'price'
         else:
-            return (0, '-price')
+            return 0, '-price'
 
 
 def sorted_by(key):
@@ -43,15 +45,29 @@ def get_view_type(view_type):
         view_type = view_type.get('view_type')
         if type(view_type) == str:
             if view_type == '1':
-                return (1, 'advertisement_page_type_2.html')
+                return 1, 'advertisement_page_type_2.html'
             else:
-                return (0, 'advertisement_page_type_1.html')
+                return 0, 'advertisement_page_type_1.html'
         elif view_type.get('view_type'):
             if view_type.get('view_type') == '0':
-                return (1, 'advertisement_page_type_2.html')
+                return 1, 'advertisement_page_type_2.html'
             else:
-                return (0, 'advertisement_page_type_1.html')
+                return 0, 'advertisement_page_type_1.html'
         else:
-            return (0, 'advertisement_page_type_1.html')
+            return 0, 'advertisement_page_type_1.html'
     else:
-        return (0, 'advertisement_page_type_1.html')
+        return 0, 'advertisement_page_type_1.html'
+
+
+def get_region_variables(region_request):
+    if region_request:
+        region_filter = dict(
+            region__in=Region.objects.filter(id=region_request).get_descendants(include_self=True))
+        region_param = region_filter.get("region__in").get(id=region_request)
+        region_bread_crumbs = region_param.get_ancestors(ascending=False, include_self=True)
+        return region_filter, region_param, region_bread_crumbs
+    else:
+        region_filter = dict(region__in=Region.objects.all())
+        region_bread_crumbs = ''
+        region_param = ''
+        return region_filter, region_param, region_bread_crumbs
--- advertisement/views.py
+++ advertisement/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Advertisement, Category, Region
 
-from .utils import sorted_by_number, variables_for_paginator, sorted_by_date_or_price, sorted_by, get_view_type
+from .utils import sorted_by_number, variables_for_paginator, sorted_by_date_or_price, sorted_by, get_view_type, \
+    get_region_variables
 
 
 # Create your views here.
@@ -13,6 +14,7 @@ def get_advertisement_page(request):
     sort_for_paginator = sorted_by_number(request.COOKIES.get('sort'))
     state_sort_by_date = request.COOKIES.get('date', 0)
     view_type, html = get_view_type(request.COOKIES)
+    region_filter, region_param, region_bread_crumbs = get_region_variables(request.GET.get('region'))
 
     if request.GET.get('date') or request.GET.get('price'):
         state_sort_by_date, order_by = sorted_by_date_or_price(request.GET)
@@ -23,21 +25,28 @@ def get_advertisement_page(request):
 
     category_list = Category.objects.filter(level__lte=1)
     advertisement_queryset = Advertisement.objects.filter(is_active=True,
-                                                          moderated=True).select_related(
+                                                          moderated=True, **region_filter).select_related(
                                                           'category',
                                                           'region').order_by(order_by)
     category_queryset = Category.objects.add_related_count(Category.objects.root_nodes(),
                                                            Advertisement,
                                                            'category',
                                                            'advertisement_counts',
-                                                           cumulative=True)
+                                                           cumulative=True,
+                                                           extra_filters={"region__in": region_filter['region__in']})
+
+
 
     page_obj = variables_for_paginator(advertisement_queryset,
                                        request.GET.get('page'),
                                        sort_for_paginator)
+
     context = {
+        "ads_found": advertisement_queryset.count(),
         "category": category_queryset,
         "category_list": category_list,
+        "region_bread_crumbs": region_bread_crumbs,
+        "region_param": region_param,
         "page_obj": page_obj,
         'date': state_sort_by_date,
         'view_type': view_type,
@@ -57,6 +66,7 @@ def get_advertisement_by_category(request, category_slug):
     sort_for_paginator = sorted_by_number(request.COOKIES.get('sort'))
     state_sort_by_date = request.COOKIES.get('date', 0)
     view_type, html = get_view_type(request.COOKIES)
+    region_filter, region_param, region_bread_crumbs = get_region_variables(request.GET.get('region'))
 
     if request.GET.get('date') or request.GET.get('price'):
         state_sort_by_date, order_by = sorted_by_date_or_price(request.GET)
@@ -73,10 +83,12 @@ def get_advertisement_by_category(request, category_slug):
                                                               Advertisement,
                                                               'category',
                                                               'advertisement_counts',
-                                                              cumulative=True)
+                                                              cumulative=True,
+                                                              extra_filters={"region__in": region_filter['region__in']})
     category_queryset = category_queryset_an.filter(parent_id=category.id)
     advertisement_queryset = Advertisement.objects.filter(Q(category__in=category_queryset_an) |
                                                           Q(category__slug=category.slug),
+                                                          **region_filter,
                                                           is_active=True,
                                                           moderated=True).select_related(
                                                           'category',
@@ -85,8 +97,11 @@ def get_advertisement_by_category(request, category_slug):
                                        request.GET.get('page'),
                                        sort_for_paginator)
     context = {
+        "ads_found": advertisement_queryset.count(),
         "category": category_queryset,
+        "region_param": region_param,
         "category_bread_crumbs": category_bread_crumbs,
+        "region_bread_crumbs": region_bread_crumbs,
         "category_list": category_list,
         "page_obj": page_obj,
         'date': state_sort_by_date,
