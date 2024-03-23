5aed99df350cf389379f4d6968e394fc3b449c1e
Igor Kartel <igor.v.kartel@gmail.com>
2024-03-23 12:00:39 +0300

    Добавлен раздел Магазины

--- main_page_domer/urls.py
+++ main_page_domer/urls.py
@@ -5,6 +5,7 @@ from main_page_domer.views import *
 urlpatterns = [
     path('', get_main_page, name='home'),
     path('stores/', get_stores_page, name='stores'),
-    # path('stores/<slug:store_slug>/', get_store_by_category, name='store_by_category'),
+    # path('stores/<slug:category_slug>/', get_store_by_category, name='store_by_category'),
+    # path('stores/<slug:store_slug>/', get_store_by_title, name='store_by_title'),
     path('help/', get_help_page, name='help'),
 ]
