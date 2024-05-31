9d246dc73e9eb59768fd246eeeea79914c965343
Oliver1ck <artem131103@yandex.ru>
2024-05-31 19:08:05 +0300

    fix add

--- advertisement/utils.py
+++ advertisement/utils.py
@@ -4,13 +4,13 @@ from .models import Region
 
 
 def sorted_by_number(number):
-    if (number == '30'
-            or number == '60'
-            or number == '90'):
+    if (number == '3'
+            or number == '6'
+            or number == '9'):
         sort_for_paginator = int(number)
         return sort_for_paginator
     else:
-        return 30
+        return 3
 
 
 def variables_for_paginator(queryset, page=1, elements=30):
