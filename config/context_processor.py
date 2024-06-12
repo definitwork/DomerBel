7b041c96a6e9fb3c6c16fdf024984551ae6d5057
Santrok <a.povedaiko@yandex.ru>
2024-06-12 19:50:26 +0300

    added reset password

--- config/context_processor.py
+++ config/context_processor.py
@@ -1,13 +1,8 @@
-from users.forms import LoginForm, RegisterForm, EmailResetForm, RegisterFormEntity
 from config.settings import env_keys
 
 
 def get_context_data(request):
     context = {
-        'login_form': LoginForm(),
-        'register_form': RegisterForm(),
-        'register_form_entity': RegisterFormEntity(),
-        'email_reset_form': EmailResetForm(),
         "public_key" : env_keys.get('RECAPTCHA_PUBLIC_KEY'),
         "url": env_keys.get('URL'),
     }
