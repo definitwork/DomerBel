547cd68b60ad6040556f008cc52aee009b38a685
Lenapanasyuk <alena01049819@gmail.com>
2024-07-15 20:19:26 +0300

    bulk_import_v3

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('favorites/', get_page_place_an_favorites, name='favorites'),
     path('import_bulk_of_ads/',get_bulk_import_of_ads),
     path('advertisement_details/<int:id>/', get_advertisement_details_page, name='advertisement_details'),
-    # path('download_file_with_error_ads/',download_file_with_error_ads),
     path('instructions_for_bulk_import_of_ads', get_instructions_for_bulk_import_of_ads),
     path('advertisement_details/<str:slug>/', get_advertisement_details_page, name='advertisement_details'),
     path('editing_an_ad/<int:id>/', editing_an_ad, name='editing_an_ad'),
