bafae42efdae2a5c4244b04719755bf834c234e1
Artsem Yunchyts <artem131103@yandex.ru>
2024-07-24 12:14:28 +0300

    test

--- main_page_domer/static/js/main/search.js
+++ main_page_domer/static/js/main/search.js
@@ -5,68 +5,86 @@ function getOption(url) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      if(data.length === 0) return false
-      if(data[0].spisok && fields.children.length !== 0) {
-        data.forEach(item => {
-          if(item?.search.split("|")?.length >= 2 && item?.search.split("|")[0].trim() !== "Марка") {
+      if (data.length === 0) return false
+      if (data[0].spisok && fields.children.length !== 0) {
+        data.forEach((item) => {
+          console.log(item);
+          if (
+            item?.search.split("|")?.length >= 2 
+            &&
+            !item?.spisok?.element_set
+          ) {
             createSelectElement(item, item.search.split("|")[0])
             createSelectElement(item, item.search.split("|")[1])
             return
           }
+          if (item.search.trim() === "") return
           createSelectElement(item, item.search.split("|")[0].trim())
         })
         return
       }
       createSelectElement(data)
-      
     })
     .catch((error) => {
       console.log(error, "error obj")
     })
 }
 
+function getInnerList(data, id) {
+  const dataModel = data.spisok.element_set.find((item) => item.id == id)
+  console.log(data, 'datamodel');
+  createSelectElement(dataModel, data.search.split("|")[1].trim())
+}
+
 function createSelectElement(
-    data, 
-    titleObj = {
-      id: "",
-      title: "Все разделы",
+  data,
+  titleObj = {
+    id: "",
+    title: "Все разделы",
   }
 ) {
   const select = document.createElement("select")
   select.setAttribute("name", "category__in")
   createOptionElement(titleObj, select)
-  
-  if(data.spisok && data.spisok !== null) {
+  if (data?.spisok && data?.spisok !== null) {
     data.spisok.element_set.forEach((item) => {
       createOptionElement(item, select)
     })
-    if(titleObj === "Марка") {
+    if (data.spisok.element_set[0]?.elementtwo_set) {
       select.addEventListener("change", (event) => {
         const id = event.target.value
-        getModel(data, id)
+        getInnerList(data, id)
       })
     }
-  }else if (!data.spisok && !data.int_val_list) {
+  } else if (!data?.spisok && !data?.int_val_list && !data.elementtwo_set) {
     data.forEach((item) => {
-      console.log(item);
       createOptionElement(item, select)
     })
     select.addEventListener("change", (event) =>
       getCategoryFunc(event, getOption)
     )
-  }else {
-    data.int_val_list.forEach(item => {
+  } else if (data?.elementtwo_set) {
+    select.classList.add("category__mark")
+    data?.elementtwo_set.forEach((item) => {
+      createOptionElement(item, select)
+    })
+    
+    fields.children[
+      Array.from(fields.children).findIndex((item) =>
+        item.classList.contains("category__mark")
+      )
+    ]?.remove()
+    console.log(fields.children);
+
+    fields.insertBefore(select, fields.children[2])
+    return
+  } else {
+    data?.int_val_list.forEach((item) => {
       createOptionElement(item, select)
     })
   }
 
   fields.append(select)
-}
-
-function getModel(id,data) {
-  console.log(data, id);
-  const dataModel = new Map(data.spisok)
-  console.log(dataModel.size);
 }
 
 function createOptionElement(item, parentElement) {
@@ -83,13 +101,13 @@ getCategory.addEventListener("change", (event) => {
 
 function getCategoryFunc(event, func) {
   const id = event.target.value
-  if(id === "") {
+  if (id === "") {
     Array.from(fields.children).forEach((item, index) => {
-      if(index !== 0) item.remove()
+      if (index !== 0) item.remove()
     })
     return
   }
-  if(!func(`http://127.0.0.1:8000/api/v1/categories_for_search/${id}`)) {
+  if (!func(`http://127.0.0.1:8000/api/v1/categories_for_search/${id}`)) {
     func(`http://127.0.0.1:8000/api/v1/get_field_list/?id=${id}`)
     return
   }
