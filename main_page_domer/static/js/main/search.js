b4ab5d573146c2ba8f2eb4edf029f7925cec9dd4
Artsem Yunchyts <artem131103@yandex.ru>
2024-07-29 13:43:59 +0300

    test

--- main_page_domer/static/js/main/search.js
+++ main_page_domer/static/js/main/search.js
@@ -25,7 +25,7 @@ function getOption(url) {
       }
       if (data.some(item => item.search === "")) return
       console.log(data);
-      createSelectElement(data, Array.isArray(data) && data.some(item => item.area) ? "Любое расположение" : {id: "", title: "Все разделы"})
+      createSelectElement(data, Array.isArray(data) && data.some(item => item.area) ? "Любое расположение" : {id: "", title: ""})
     })
     .catch((error) => {
       console.error(error, "error obj")
