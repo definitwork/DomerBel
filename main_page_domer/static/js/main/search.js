515081ab4cda938f63947926bf16f2862a7c77a5
Artsem Yunchyts <artem131103@yandex.ru>
2024-07-29 13:38:42 +0300

    fix search location

--- main_page_domer/static/js/main/search.js
+++ main_page_domer/static/js/main/search.js
@@ -25,7 +25,7 @@ function getOption(url) {
       }
       if (data.some(item => item.search === "")) return
       console.log(data);
-      createSelectElement(data, Array.isArray(data) && data.some(item => item.area) ? "Любое расположение" : null)
+      createSelectElement(data, Array.isArray(data) && data.some(item => item.area) ? "Любое расположение" : 'Все разделы')
     })
     .catch((error) => {
       console.error(error, "error obj")
