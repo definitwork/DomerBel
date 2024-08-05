ac3375baeb15938167e10536de0baca199559705
Artsem Yunchyts <artem131103@yandex.ru>
2024-08-05 17:31:03 +0300

    add errors in forms

--- main_page_domer/static/js/profile/region.js
+++ main_page_domer/static/js/profile/region.js
@@ -1,6 +1,11 @@
 const regionSelect = document.querySelector(".main__info select[name='region']")
 let regionList = document.querySelectorAll(".main__info select[name='region']")
 regionSelect?.addEventListener("change", (event) => {
+  if(event.target.value === "") {
+    regionList = document.querySelectorAll(".main__info select[name='region']")
+    Array.from(regionList).splice(1,regionList.length).forEach(region => region.remove())
+    return
+  }
   fetch(`http://127.0.0.1:8000/api/v1/get_city_list/${event.target.value}`)
     .then(resp => resp.json())
     .then(data => {
