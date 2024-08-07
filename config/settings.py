ae5bd4b69c641b6bf0fb6de55cab3e93d9c1be2f
Alexander <a.povedaiko@yandex.ru>
2024-08-07 15:46:08 +0300

    finish changed store

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ SECRET_KEY = env_keys.get('DJANGO_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
---
+++ users/signals.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import Group
+from django.db import transaction
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from users.models import User
+
+
+@receiver(post_save, sender=User)
+def add_user_in_permission_group(sender: User, instance: User, **kwargs):
+    """Добавление пользователя в группу
+    с правами доступными только юр. лицам"""
+    group = Group.objects.get(name='Юридические лица')
+    if instance.entity:
+        transaction.on_commit(lambda: instance.groups.add(group))
