147cc140a8d1ad3510e5cbd7e9ee6577580fba2e
Artsem Yunchyts <artem131103@yandex.ru>
2024-08-10 14:50:46 +0300

    hidden

--- main_page_domer/static/js/main/login.js
+++ main_page_domer/static/js/main/login.js
@@ -6,13 +6,26 @@ modalsBlock.addEventListener('click', (event) => {
     if(event.target === modalsBlock){
         modalsBlock.classList.remove('modal__active');
         for(let i of modalsBlock.children) {
-            i.classList.remove('modal__active');
-            i.classList.remove("modals__active-grid")
+            i.classList.remove('modal__active') || i.classList.remove("modals__active-grid");
+           
             const fieldErorr = document.querySelectorAll('.modals__signIn-error')
             fieldErorr.forEach(item => item.remove())
         }
         document.body.style.overflow = 'auto';
     } 
+})
+
+document.addEventListener("keyup", (event) => {    
+    if (event.code === "Escape") {
+        modalsBlock.classList.remove('modal__active');
+        for(let i of modalsBlock.children) {
+            i.classList.remove('modal__active') || i.classList.remove("modals__active-grid");
+        }
+        document.body.style.overflow = 'auto';  
+    }
+    // if(event.code === "Enter") {
+    //     document.querySelector('.modals .modal__active')?.submit()
+    // }
 })
 
 loginBtn?.addEventListener('click', () => {
